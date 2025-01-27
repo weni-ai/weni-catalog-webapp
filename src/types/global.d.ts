12b475e89a8aa2d4993f6c78c8b4f6a271110a40
acnormun <anaclara.noronha@weni.ai>
2025-01-27 14:55:37 -0300

    chore: config sentry

---
+++ src/types/global.d.ts
@@ -0,0 +1,3 @@
+interface Window {
+  configs?: { [key: string]: string };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    __APP_NAME__: JSON.stringify(process.env.npm_package_name),
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   css: {
     preprocessorOptions: {
       scss: {
