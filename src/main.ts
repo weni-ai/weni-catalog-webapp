6d256662bd88c9b523b39aadb7a32a6e0c16cead
acnormun <anaclara.noronha@weni.ai>
2025-01-27 15:14:20 -0300

    chor: set router

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
 import { createPinia } from 'pinia'
 import { UnnnicSystem } from './plugins/UnnnicSystem';
+import { i18n } from './locales'
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia)
+app.use(router)
+app.use(i18n)
 app.use(UnnnicSystem)
 
 app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>aloo</p>
+</template>
