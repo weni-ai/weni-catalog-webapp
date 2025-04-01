88b8c072611b658a1fa7f7d4a241b0a14b5632f8
acnormun <anaclara.noronha@weni.ai>
2025-04-01 16:02:23 -0300

    fix: add semantic html

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,37 +1,42 @@
 <template>
-    <div class="inventory-view">
-        <div class="inventory-view__header">
+    <section class="inventory-view">
+        <header class="inventory-view__header">
             <p class="inventory-view__header__title">Este produto possui poucos itens em estoque</p>
             <p class="inventory-view__header__subtitle">Se faltar, o que você deseja fazer?</p>
-        </div>
+        </header>
 
-        <div class="inventory-view__content">
-            <div v-if="similarProducts.length > 0" class="inventory-view__content__items"
-                v-for="item in similarProducts" :key="item.id">
-                <div class="inventory-view__content__items__title">
+        <section class="inventory-view__content">
+            <article
+                v-if="similarProducts.length > 0"
+                class="inventory-view__content__items"
+                v-for="item in similarProducts"
+                :key="item.id"
+            >
+                <header class="inventory-view__content__items__title">
                     <unnnicRadio class="inventory-view__content__items__title__radio" :value="item.id" v-model="selectedItem">
                         <p class="inventory-view__content__items__title__radio__text">Alterar produto para</p>
                     </unnnicRadio>
-                </div>
+                </header>
                 <CartItem :product="{ item, qtd: 0 }" />
-            </div>
-        </div>
-        <div class="inventory-view__replace">
+            </article>
+        </section>
+
+        <section class="inventory-view__replace">
             <div class="inventory-view__replace__radio">
                 <unnnicRadio value="leave" v-model="selectedItem" />
                 <p class="inventory-view__replace__title">Deixar a loja substituir</p>
             </div>
             <p class="inventory-view__replace__subtitle">A loja vai substituir por um produto similar</p>
-        </div>
+        </section>
 
-        <div class="inventory-view__refund">
+        <section class="inventory-view__refund">
             <div class="inventory-view__refund__radio">
                 <unnnicRadio value="refund" v-model="selectedItem" />
                 <p class="inventory-view__refund__title">Reembolsar valor</p>
             </div>
             <p class="inventory-view__refund__subtitle">A loja vai reembolsar o valor do produto</p>
-        </div>
-    </div>
+        </section>
+    </section>
 </template>
 
 <script setup lang="ts">
@@ -57,47 +62,6 @@ const selectedItem = ref<ProductItem>(props.product.item);
     gap: 16px;
     font-family: $unnnic-font-family-secondary;
     height: 100%;
-    &__content {
-        display: flex;
-        flex-direction: column;
-        gap: 16px;
-        @media screen and (min-width: $tablet-width) {
-            flex-direction: row;
-        }
-        &__items {
-            display: flex;
-            flex: 1;
-            flex-direction: column;
-            padding: 16px;
-            border: 1px solid $unnnic-color-neutral-soft;
-            border-radius: $unnnic-border-radius-sm;
-
-            &__item {
-                display: flex;
-                flex-direction: column;
-                gap: 16px;
-            }
-
-            @media screen and (min-width: $tablet-width) {
-                &__title {
-                    display: flex;
-                }
-            }
-            &__title {
-                &__radio {
-                    &__text {
-                        font-weight: bold;
-                        color: $unnnic-color-neutral-black;
-                    }
-                }
-    
-                @media (min-width: $tablet-width) {
-                    display: flex;
-                }
-            }
-        }
-    }
-
 
     &__header {
         display: flex;
@@ -105,7 +69,6 @@ const selectedItem = ref<ProductItem>(props.product.item);
         justify-content: center;
         align-items: center;
         gap: 8px;
-        font-family: $unnnic-font-family-secondary;
 
         @media (min-width: $tablet-width) {
             align-items: flex-start;
@@ -123,11 +86,51 @@ const selectedItem = ref<ProductItem>(props.product.item);
         }
     }
 
+    &__content {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+
+        @media screen and (min-width: $tablet-width) {
+            flex-direction: row;
+        }
+
+        &__items {
+            display: flex;
+            flex: 1;
+            flex-direction: column;
+            padding: 16px;
+            border: 1px solid $unnnic-color-neutral-soft;
+            border-radius: $unnnic-border-radius-sm;
+
+            &__item {
+                display: flex;
+                flex-direction: column;
+                gap: 16px;
+            }
+
+            &__title {
+                display: flex;
+
+                &__radio {
+                    &__text {
+                        font-weight: bold;
+                        color: $unnnic-color-neutral-black;
+                    }
+                }
+
+                @media (min-width: $tablet-width) {
+                    display: flex;
+                }
+            }
+        }
+    }
+
     &__replace,
     &__refund {
         display: flex;
         flex-direction: column;
-        border: solid 1px $unnnic-color-neutral-soft;
+        border: 1px solid $unnnic-color-neutral-soft;
         border-radius: $unnnic-border-radius-sm;
         padding: $unnnic-spacing-xs;
 
@@ -139,20 +142,19 @@ const selectedItem = ref<ProductItem>(props.product.item);
 
         &__title {
             font-size: $unnnic-font-size-body-gt;
-            color: $unnnic-color-neutral-darkest;
             font-weight: bold;
+            color: $unnnic-color-neutral-darkest;
         }
 
         &__subtitle {
-            color: $unnnic-color-neutral-cloudy;
             font-size: $unnnic-font-size-body-md;
+            color: $unnnic-color-neutral-cloudy;
             padding-left: 40px;
 
             @media (min-width: $tablet-width) {
                 display: flex;
             }
         }
-
     }
 }
 </style>
