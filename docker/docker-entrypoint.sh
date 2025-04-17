0bd15c57b086ee3d0b6b25002ddf60ce35d20edd
Adenn Tumba <adinn.tumba@hotmail.com>
2025-04-17 17:53:16 -0300

    uncomment args old_image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,6 @@ export JSON_STRING='window.configs = { \
   "SENTRY_ENVIRONMENT":"'${SENTRY_ENVIRONMENT}'", \
   "API_BASE_URL":"'${API_BASE_URL}'", \
 }'
-sed "s|\/\/ CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/weni-catalog-webapp/index.html.tmpl > /tmp/index.html
+sed "s|\/\/ CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/catalog/index.html.tmpl > /tmp/index.html
 
 exec "$@"
